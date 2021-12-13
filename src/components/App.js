e50249c18d8a3a8e6e8e0229e612feebbdfe19ec
이호성 <212366@patner.sk.com>
2021-12-13 18:16:07 +0900

    셋팅중'

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import React,{useState,useEffect} from 'react';
-import './App.css';
+import '../css/App.css';
 
 const App = () =>  {
   const [count, setCount] =useState(0);
@@ -12,14 +11,18 @@ const App = () =>  {
   })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
+      <header className="header">
+        헤더영역
+      </header>
+      <body>
         <h3>현재 카운트 = {count}</h3>
         <button onClick={()=> setCount(count+1)}>카운트증가</button>
         <button onClick={()=> setCount(count-1)}>카운트감소</button>
         <button onClick={countChk}>현재 카운트 팝업</button>
-      </header>
+      </body>
+      <footer>
+        푸터
+      </footer>
     </div>
   );
 }
